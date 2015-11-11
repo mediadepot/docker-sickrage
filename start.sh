32ff05aee4a4e75d9abfe1ef34eefd975ffc6b84
Jason Kulatunga <jk17@ualberta.ca>
2015-11-11 13:59:01 -0800

    update sickrage.

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 if [ ! -f /srv/sickrage/config/sickbeard.ini ]; then
 
 	cheetah fill --oext ini --env /src/sickrage/config/sickbeard
-	chown -R depot:depot /srv/couchpotato
+	chown -R depot:depot /srv/sickrage
 fi
 
 su -c "/usr/bin/python /srv/sickrage/app/SickBeard.py --nolaunch --datadir /srv/sickrage/data --config /srv/sickrage/config/sickbeard.ini" depot
