f75c3d13548dd3826b462f0d6ed9d51188264b2a
Jason Kulatunga <jason@thesparktree.com>
2018-11-21 17:58:33 -0800

    use SickChill not sickrage.

--- start.sh
+++ start.sh
@@ -7,7 +7,7 @@ sh /srv/config.sh
 chown mediadepot:mediadepot -R /srv/sickrage
 
 # download the latest version of SickRage
-[[ ! -d /srv/sickrage/app/.git ]] && su -c "git clone https://github.com/SickRage/SickRage.git /srv/sickrage/app" mediadepot
+[[ ! -d /srv/sickrage/app/.git ]] && su -c "git clone https://github.com/SickChill/SickChill.git /srv/sickrage/app" mediadepot
 
 # opt out for autoupdates using env variable
 if [ -z "$ADVANCED_DISABLEUPDATES" ]; then
